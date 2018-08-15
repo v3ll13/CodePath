43f285f6ce4f93f63b535b68a5351cfd27e0b5b9
v3ll13 <laguerre.vellie@gmail.com>
2018-08-14 20:03:48 -0400

    pushing nbpharma

---
+++ NBpharma/app/src/main/java/ht/queeny/nbpharma/LoginActivity.java
@@ -0,0 +1,58 @@
+package ht.queeny.nbpharma;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.text.BreakIterator;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private static final String TAG = "LoginActivity";
+    private boolean mIsResolving = false;
+    private boolean mShouldResolve = false;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.sign_in_button) {
+            Toast.makeText(LoginActivity.this, "This is my Toast message!",
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
